837954c2e6fc3f2e6fb2b7bb0598ffe25dcf4a2f
Pedro Correia <pedrogrcorreia@gmail.com>
2023-03-15 00:51:05 +0000

    creating commands with yargs

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
+const { describe } = require('yargs')
 
-const msg = getNotes()
+yargs.version('1.1.0')
 
-console.log(msg)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function (){
+        console.log('Adding a new note!')
+    }
+})
 
-console.log(validator.isURL('xample.com'))
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log("Removing a note!")
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Describe notes',
+    handler: function(){
+        console.log("Describing notes!")
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log("Reading notes!")
+    }
+})
+
+console.log(yargs.argv)
